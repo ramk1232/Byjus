5b1dbf2118a89149d79fbe99e9bf50f5395ec802
ramko <ramko@RKAINS007>
2023-08-05 22:10:59 +0530

    Changes Done

--- Byjus/src/test/java/TestWatchVid/RegisterCross.java
+++ Byjus/src/test/java/TestWatchVid/RegisterCross.java
@@ -23,59 +23,60 @@ import Byju.BookCls;
 import Byju.Regit;
 
 public class RegisterCross {
-	
+
 	WebDriver driver;
 	Regit Reg;
 	SoftAssert assertt;
 	WebDriverWait wait;
 	BookCls bokcls;
-	
-	//For Log4j
-	Logger logger = LogManager.getLogger(Register.class);   //Log4j
-	
+
+	// For Log4j
+	Logger logger = LogManager.getLogger(Register.class); // Log4j
+
 	@Parameters("browser")
-	
+
 	@BeforeTest
-		
-		public void openBrowser(String browsername) {
-			System.out.println(browsername);
-			
-			if (browsername.equals("Chrome")) {
-				
-				System.setProperty("webdriver.chrome.driver","C:\\Selenium\\chromedriver.exe");
-				driver = new ChromeDriver();	
-			}
-			
-			if (browsername.equals("Firefox")) {
-					
-				System.setProperty("webdriver.gecko.driver","C:\\Selenium\\geckodriver1.exe");
-				driver = new FirefoxDriver();
-					
-			}
-	        driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
-			driver.manage().window().maximize();
-			driver.get("https://byjus.com/byjus-classes-book-free-60-mins-class/?utm_source=google&utm_mode=CPA&utm_campaign=K10-IN-Brand-BYJU%27S-USD-Exact&utm_term=byjus&gclid=Cj0KCQjwy5maBhDdARIsAMxrkw30p2qYMoe8xOHblLDi_uHpY-rPiZwNcjD2aSjj0kksaMtQvK4RH9EaAthoEALw_wcB");
+
+	public void openBrowser(String browsername) {
+		System.out.println(browsername);
+
+		if (browsername.equals("Chrome")) {
+
+			System.setProperty("webdriver.chrome.driver", "C:\\Selenium\\chromedriver.exe");
+			driver = new ChromeDriver();
 		}
+
+		if (browsername.equals("Firefox")) {
+
+			System.setProperty("webdriver.gecko.driver", "C:\\Selenium\\geckodriver1.exe");
+			driver = new FirefoxDriver();
+
+		}
+		driver.manage().timeouts().implicitlyWait(50, TimeUnit.SECONDS);
+		driver.manage().window().maximize();
+		driver.get(
+				"https://byjus.com/byjus-classes-book-free-60-mins-class/?utm_source=google&utm_mode=CPA&utm_campaign=K10-IN-Brand-BYJU%27S-USD-Exact&utm_term=byjus&gclid=Cj0KCQjwy5maBhDdARIsAMxrkw30p2qYMoe8xOHblLDi_uHpY-rPiZwNcjD2aSjj0kksaMtQvK4RH9EaAthoEALw_wcB");
+	}
+
 	@BeforeClass
 	public void beforeClass() throws InterruptedException {
-		
+
 		Reg = new Regit(driver);
 		bokcls = new BookCls(driver);
 	}
-	
+
 	@BeforeMethod
 	public void beforeMethod() throws InterruptedException {
-		
+
 		System.out.println("before method is running");
 		Thread.sleep(3000);
-		
-		
+
 		logger.info("Entering info in Byjus Class Schedule form");
-		
+
 		Reg.childname();
 		Thread.sleep(2000);
-		Reg.entermob();	
-		Thread.sleep(1000);		
+		Reg.entermob();
+		Thread.sleep(1000);
 		Reg.email();
 		Thread.sleep(1000);
 		Reg.state();
@@ -94,10 +95,10 @@ public class RegisterCross {
 		bokcls.timslot();
 		Thread.sleep(3000);
 		bokcls.Bokcls();
-		
+
 		logger.info("Byjus Form Submitted successfully");
 	}
-	
+
 	@Test
 	public void byju() {
 		System.out.println("test method is running");
@@ -108,19 +109,19 @@ public class RegisterCross {
 
 	@AfterMethod
 	public void afterMethod() {
-					
+
 	}
+
 	@AfterClass
 	public void afterclass() {
 		Reg = null;
 		bokcls = null;
 //
 	}
+
 	@AfterTest
 	public void afterTest() {
 		driver.close();
 		System.gc();
 	}
 }
-
-
